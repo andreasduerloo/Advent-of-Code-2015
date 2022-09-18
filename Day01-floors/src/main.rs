e461d694ec3ebfc86e1361379c26239216f9dd62
andreasduerloo <andreasduerloo@gmail.com>
2022-09-18 11:54:22 +0200

    Initial solution

--- Day01-floors/src/main.rs
+++ Day01-floors/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let char_vec: Vec<&str> = input.split("").collect();
+
+    let mut floor: isize = 0;
+
+    for instruction in char_vec {
+        match instruction {
+            "(" => {
+                floor += 1;
+            },
+            ")" => {
+                floor -= 1;
+            },
+            _ => {
+                println!("Santa ends up on floor number {}", floor);
+            }
+        }
+    }
 }
