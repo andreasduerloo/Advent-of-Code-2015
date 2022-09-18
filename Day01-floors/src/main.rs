aa978b804b7d1e42e056a0784e6ac02da3b6e9b3
andreasduerloo <andreasduerloo@gmail.com>
2022-09-18 12:05:10 +0200

    First day done

--- Day01-floors/src/main.rs
+++ Day01-floors/src/main.rs
@@ -5,18 +5,26 @@ fn main() {
     let char_vec: Vec<&str> = input.split("").collect();
 
     let mut floor: isize = 0;
+    let mut character: usize = 0; // There appears to be some leading character
+    let mut basement: bool = false;
 
     for instruction in char_vec {
         match instruction {
             "(" => {
                 floor += 1;
+                character += 1;
             },
             ")" => {
                 floor -= 1;
+                character += 1;
             },
             _ => {
-                println!("Santa ends up on floor number {}", floor);
+                println!("Santa ends up on floor number {} 🎅", floor);
             }
+        }
+        if floor < 0 && !basement {
+            println!("We're in the basement for the first time at character {}", character);
+            basement = true;
         }
     }
 }
