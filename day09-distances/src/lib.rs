1b1305132eb5a1e892730b08eb8faab731acc82c
andreasduerloo <andreasduerloo@gmail.com>
2022-09-29 20:55:18 +0200

    Working on day 9

--- day09-distances/src/lib.rs
+++ day09-distances/src/lib.rs
@@ -1,5 +1,54 @@
 use std::collections::HashMap;
 
-struct Location {
-    distances: Box<HashMap<Box<Location>,usize>>
+pub struct Location<'a> {
+    pub name: &'a str,
+    pub distances: Box<HashMap<&'a str,usize>>
+}
+
+impl Location<'_> {
+    fn new(name: &str) -> Location {
+        Location {
+            name,
+            distances: Box::new(HashMap::new()),
+        }
+    }
+}
+
+pub fn add_info<'a>(mut locations: Vec<Location<'a>>, start: &'a str, destination: &'a str, distance: usize) -> Vec<Location<'a>> {   
+    if locations.len() == 0 { // We don't know anything yet
+        locations.push(Location::new(start));
+        locations.push(Location::new(destination));
+    }
+
+    let mut start_known: bool = false;
+    let mut dest_known: bool = false;
+
+    for i in 0..locations.len() {
+        if locations[i].name == start {
+            locations[i].distances.entry(destination).or_insert(distance);
+            start_known = true;
+        }
+        else if locations[i].name == destination {
+            locations[i].distances.entry(start).or_insert(distance);
+            dest_known = true;
+        }
+    }
+    if !start_known { // Would be nice to have a better Location::new that also built the HashMap
+        locations.push(Location::new(start));
+        for i in 0..locations.len() {
+            if locations[i].name == start {
+                locations[i].distances.entry(destination).or_insert(distance);
+            }
+        }
+    }
+    else if !dest_known {
+        locations.push(Location::new(destination));
+        for i in 0..locations.len() {
+            if locations[i].name == destination {
+                locations[i].distances.entry(start).or_insert(distance);
+            }
+        }
+    }
+
+    locations
 }
--- day09-distances/src/main.rs
+++ day09-distances/src/main.rs
@@ -1,6 +1,25 @@
 use std::fs;
+use day09_distances::*;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let mut input_vec: Vec<&str> = input.lines().collect();
+    let input_vec: Vec<&str> = input.lines().collect();
+
+    let mut locations: Vec<Location> = Vec::new();
+
+    for line in input_vec {
+        let elements: Vec<&str> = line.split_whitespace().collect();
+        let distance: usize = usize::from_str_radix(elements[4], 10).unwrap();
+        locations = add_info(locations, elements[0], elements[2], distance);
+    }
+
+    // println!("These are the cities we know, along with how many cities they know the distance to:");
+    // for location in locations {
+    //     println!("Name: {} - known distances: {}", location.name, location.distances.len());
+    // }
+
+    println!("These are the distances from Tristram:");
+    for distance in locations[0].distances.keys() {
+        println!("City: {}, distance: {}", distance, locations[0].distances.get(distance).unwrap());
+    }
 }
