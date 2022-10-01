b4025723edd9554a42bec0494dea6ce27b5f76f7
andreasduerloo <andreasduerloo@gmail.com>
2022-10-01 20:06:31 +0200

    Added lib file

--- day11-passwords/src/main.rs
+++ day11-passwords/src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use day11_passwords::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = env::args();
+
+    match arguments.len() {
+        0 | 1 => {
+            println!("Missing argument - starting password");
+            return
+        }
+        2 => {
+            //
+        }
+        _ => {
+            println!("Too many arguments.");
+            return
+        }
+    }
 }
